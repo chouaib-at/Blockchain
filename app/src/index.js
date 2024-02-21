d9d3a4329fe5221b19e6d5482ef42246a9dea95a
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-21 23:48:33 +0100

    installing thirdweb

---
+++ app/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import  ( BrowserRouter as Router ) from 'react-router-dom';
+import   { chainid, thirdwebProvider }  from '@thirdweb-dev/react';
