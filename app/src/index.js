672fdf99000adaa3bba9fa1b45255b1387ae3dc1
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-22 20:35:25 +0100

    building app with react

--- app/src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom/client';
-import  ( BrowserRouter as Router ) from 'react-router-dom';
-import   { chainid, thirdwebProvider }  from '@thirdweb-dev/react';
